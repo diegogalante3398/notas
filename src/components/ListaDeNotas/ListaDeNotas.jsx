7c2b06bfd30bca869db015b6a7482582ef4e6896
Diego Galante <diego.galante2010@hotmail.com>
2021-09-15 22:40:37 -0300

    Primeiro commit -> Layout com estilo e componentes separados por pasta

---
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Component } from "react";
+import CardNota from "../CardNota";
+import "./style.css"
+
+class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul className="lista-notas">
+                {
+                    /* Adicionando "Array of" para repetição dos elemento "li" */
+                    
+                    Array.of("Trabalho", "Trabalho", "Estudo")
+                        .map((categoria, index) => {
+                            return (
+                                <li className="lista-notas_item" key={index}>
+                                    <div>{categoria}</div>
+                                    <CardNota />
+                                </li>
+                            );
+                        })
+                }
+            </ul>
+        )
+    }
+}
+
+export default ListaDeNotas;
